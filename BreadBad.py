99ed29a457ae6243a783a08e19f072a1d387b03a
DtxdF <40880686+DtxdF@users.noreply.github.com>
2018-10-27 18:33:29 -0400

    Update BreadBad.py

--- BreadBad.py
+++ BreadBad.py
@@ -1,9 +1,9 @@
 # Created by: DtxdF
+# Version: 1.1
 # -*- coding: utf-8 -*-
 
-import requests
 import os
-from json import loads
+import geoip
 from time import sleep
 try:
 	from colorama import *
@@ -24,200 +24,13 @@ geoipamy = True
 
 class color:
 	
-	green = Fore.GREEN
-	yellow = Fore.YELLOW
-	red = Fore.RED
-	ble = Fore.BLUE+"["+Fore.RESET+"*"+Fore.BLUE+"]"+Fore.RESET+" "
-	adv = Fore.YELLOW+"["+Fore.RESET+"!"+Fore.YELLOW+"]"+Fore.RESET+" "
-	error = Fore.RED+"["+Fore.RESET+"-"+Fore.RED+"]"+Fore.RESET+" "
+	green = Fore.LIGHTGREEN_EX
+	yellow = Fore.LIGHTYELLOW_EX
+	red = Fore.LIGHTRED_EX
+	ble = Fore.LIGHTBLUE_EX+"["+Fore.RESET+"*"+Fore.LIGHTBLUE_EX+"]"+Fore.RESET+" "
+	adv = Fore.LIGHTYELLOW_EX+"["+Fore.RESET+"!"+Fore.LIGHTYELLOW_EX+"]"+Fore.RESET+" "
+	error = Fore.LIGHTRED_EX+"["+Fore.RESET+"-"+Fore.LIGHTRED_EX+"]"+Fore.RESET+" "
 	reset = Fore.RESET
-	
-class geoip:
-
-	def __init__(self, ipaddr, localsh=False):
-		
-		if not localsh:
-			self.ipaddr = ipaddr
-		else:
-			self.ipaddr = ""
-		
-	def locate(self):
-		
-		try:
-		
-			get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-			
-			ass = loads(get_geo)['as']
-			country = loads(get_geo)['country']
-			city = loads(get_geo)['city']
-			countryCode = loads(get_geo)['countryCode']
-			isp = loads(get_geo)['isp']
-			lat = loads(get_geo)['lat']
-			lon = loads(get_geo)['lon']
-			org = loads(get_geo)['org']
-			query = loads(get_geo)['query']
-			region = loads(get_geo)['region']
-			regionName = loads(get_geo)['regionName']
-			timezone = loads(get_geo)['timezone']
-			
-			list_isp_query = {country,city,countryCode,isp,org,lat,lon,query,ass,region,regionName,timezone}
-			
-			for i in list_isp_query:
-				if i == '':
-					i = None
-			
-			print "\n"
-			print "\tCountry(%s) \t\t:: %s" % (str(countryCode.encode("utf-8")),str(country.encode("utf-8")))
-			print "\tCity \t\t\t:: %s" % (str(city.encode("utf-8")))
-			print "\tISP \t\t\t:: %s" % (str(isp.encode("utf-8")))
-			print "\tOrganization \t\t:: %s" % (str(org.encode("utf-8")))
-			print "\tLatitude \t\t:: %s" % (str(lat))
-			print "\tLongitude \t\t:: %s" % (str(lon))
-			print "\tQuery \t\t\t:: %s" % (str(query.encode("utf-8")))
-			print "\tAssociation \t\t:: %s" % (str(ass.encode("utf-8")))
-			print "\tRegion \t\t\t:: %s" % (str(region.encode("utf-8")))
-			print "\tRegion Name \t\t:: %s" % (str(regionName.encode("utf-8")))
-			print "\tTime Zone \t\t:: %s" % (str(timezone.encode("utf-8")))
-			print "\n"
-				
-		except requests.exceptions.ConnectionError:
-			print color.error+"Error in the connection ..."
-		except requests.exceptions.MissingSchema:
-			print color.adv+"I miss the protocol"
-		except Exception as a:
-			print color.error+"Unknown error: %s" % str(a)
-			
-	def country(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		country = loads(get_geo)['country'].encode("utf-8")
-		
-		if country == '':
-			country = None
-			
-		return country
-			
-	def city(self):
-		
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		city = loads(get_geo)['city'].encode("utf-8")
-		
-		if city == '':
-			city = None
-			
-		return city
-		
-	def code(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		countryCode = loads(get_geo)['countryCode'].encode("utf-8")
-		
-		if countryCode == '':
-			countryCode = None
-			
-		return countryCode
-		
-	def isp(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		isp = loads(get_geo)['isp'].encode("utf-8")
-		
-		if isp == '':
-			isp = None
-			
-		return isp
-		
-	def org(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		org = loads(get_geo)['org'].encode("utf-8")
-		
-		if org == '':
-			org = None
-			
-		return org
-		
-	def lat(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		lat = loads(get_geo)['lat']
-		
-		if lat == '':
-			lat = None
-			
-		return lat
-		
-	def lon(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		lon = loads(get_geo)['lon']
-		
-		if lon == '':
-			lon = None
-			
-		return lon
-		
-	def query(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		query = loads(get_geo)['query'].encode("utf-8")
-		
-		if query == '':
-			query = None
-			
-		return query
-		
-	def aso(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		ass = loads(get_geo)['as'].encode("utf-8")
-		
-		if ass == '':
-			ass = None
-			
-		return ass
-		
-	def region(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		region = loads(get_geo)['region'].encode("utf-8")
-		
-		if region == '':
-			region = None
-			
-		return region
-		
-	def regionName(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		regionName = loads(get_geo)['regionName'].encode("utf-8")
-		
-		if regionName == '':
-			regionName = None
-			
-		return regionName
-		
-	def timezone(self):
-	
-		get_geo = requests.get("http://ip-api.com/json/"+self.ipaddr).text
-		
-		timezone = loads(get_geo)['timezone'].encode("utf-8")
-		
-		if timezone == '':
-			timezone = None
-			
-		return timezone
 
 print "\n"
 		
@@ -229,7 +42,7 @@ print "Help Main:"
 print "#### ####"
 
 print """
-	IPAdress \t::\t The command to define the ip to geolocate
+	IPAddress \t::\t The command to define the ip to geolocate
 	Country \t::\t See the country of the defined ip
 	City \t\t::\t See the city of the defined ip
 	Country_Code \t::\t See the country code of the defined ip
@@ -242,12 +55,59 @@ print """
 	region \t\t::\t See the region of the defined ip
 	region_name \t::\t See the name belonging to the defined ip
 	TimeZone \t::\t See the time zone of the defined ip
+	Google_Maps \t::\t Show the link of google maps
+	all_information ::\t Show the all information from the ip address
 """
 		
 print "\nGeolocation to you:"
 print "########### ## ###"
 		
-geoip(ipaddr=ipadress,localsh=geoipamy).locate()
+try:
+		
+	get_geo = geoip.geolocation("localme")
+	get_geo.start()
+			
+	ass = get_geo.association()
+	country = get_geo.country()
+	city = get_geo.city()
+	countryCode = get_geo.countryCode()
+	isp = get_geo.isp()
+	lat = get_geo.latitude()
+	lon = get_geo.longitude()
+	org = get_geo.organization()
+	query = get_geo.query()
+	region = get_geo.region()
+	regionName = get_geo.regionName()
+	timezone = get_geo.timezone()
+	zipcode = get_geo.zipCode()
+	google_maps = get_geo.google_maps()
+	
+	list_isp_query = {country,city,countryCode,isp,org,lat,lon,query,ass,region,regionName,timezone,zipcode,google_maps}
+			
+	for i in list_isp_query:
+		if i == '':
+			i = None
+			
+	print "\n"
+	print "\tCountry(%s) \t\t:: %s" % (str(countryCode),str(country))
+	print "\tCity \t\t\t:: %s" % (str(city))
+	print "\tISP \t\t\t:: %s" % (str(isp))
+	print "\tOrganization \t\t:: %s" % (str(org))
+	print "\tLatitude \t\t:: %s" % (str(lat))
+	print "\tLongitude \t\t:: %s" % (str(lon))
+	print "\tQuery \t\t\t:: %s" % (str(query))
+	print "\tAssociation \t\t:: %s" % (str(ass))
+	print "\tRegion \t\t\t:: %s" % (str(region))
+	print "\tRegion Name \t\t:: %s" % (str(regionName))
+	print "\tTime Zone \t\t:: %s" % (str(timezone))
+	print "\tZip Code \t\t:: %s" % (str(zipcode))
+	print "\tGoogle maps \t\t:: %s" % (str(google_maps))
+	print "\n"
+				
+except EOFError:
+	print color.adv+"Error in the connection ..."
+except Exception as a:
+	print color.error+"Unknown error: %s" % str(a)
 		
 while True:
 	
@@ -258,35 +118,85 @@ while True:
 		if not debug:
 			continue
 			
-		if debug.split()[0].lower() == 'ipadress':
+		if debug.split()[0].lower() == 'ipaddress':
 			ipadress = debug.split()[1]
-			geoip(ipaddr=ipadress).locate()
+			iptolocate = geoip.geolocation(ipadress)
+			try:
+				iptolocate.start()
+			except geoip.statusip as a:
+				print color.error+"Error: %s" % (str(a))
 		
 		if ipadress:
 			if debug.split()[0].lower() == 'country':
-				print color.ble+"\tCountry: %s" % color.green+str(geoip(ipaddr=ipadress).country())+color.reset
+				print color.ble+"\tCountry: %s" % color.green+str(iptolocate.country())+color.reset
 			elif debug.split()[0].lower() == 'city':
-				print color.ble+"\tCity: %s" % color.green+str(geoip(ipaddr=ipadress).city())+color.reset
+				print color.ble+"\tCity: %s" % color.green+str(iptolocate.city())+color.reset
 			elif debug.split()[0].lower() == 'country_code':
-				print color.ble+"\tCountry Code: %s" % color.green+str(geoip(ipaddr=ipadress).code())+color.reset
+				print color.ble+"\tCountry Code: %s" % color.green+str(iptolocate.countryCode())+color.reset
 			elif debug.split()[0].lower() == 'isp':
-				print color.ble+"\tISP: %s" % color.green+str(geoip(ipaddr=ipadress).isp())+color.reset
+				print color.ble+"\tISP: %s" % color.green+str(iptolocate.isp())+color.reset
 			elif debug.split()[0].lower() == 'org':
-				print color.ble+"\tOrganization: %s" % color.green+str(geoip(ipaddr=ipadress).org())+color.reset
+				print color.ble+"\tOrganization: %s" % color.green+str(iptolocate.organization())+color.reset
 			elif debug.split()[0].lower() == 'latitude':
-				print color.ble+"\tLatitude: %s" % color.green+str(geoip(ipaddr=ipadress).lat())+color.reset
+				print color.ble+"\tLatitude: %s" % color.green+str(iptolocate.latitude())+color.reset
 			elif debug.split()[0].lower() == 'longitude':
-				print color.ble+"\tLongitude: %s" % color.green+str(geoip(ipaddr=ipadress).lon())+color.reset
+				print color.ble+"\tLongitude: %s" % color.green+str(iptolocate.longitude())+color.reset
 			elif debug.split()[0].lower() == 'query':
-				print color.ble+"\tQuery: %s" % color.green+str(geoip(ipaddr=ipadress).query())+color.reset
+				print color.ble+"\tQuery: %s" % color.green+str(iptolocate.query())+color.reset
 			elif debug.split()[0].lower() == 'association':
-				print color.ble+"\tAssociation: %s" % color.green+str(geoip(ipaddr=ipadress).aso())+color.reset
+				print color.ble+"\tAssociation: %s" % color.green+str(iptolocate.association())+color.reset
 			elif debug.split()[0].lower() == 'region':
-				print color.ble+"\tRegion: %s" % color.green+str(geoip(ipaddr=ipadress).region())+color.reset
+				print color.ble+"\tRegion: %s" % color.green+str(iptolocate.region())+color.reset
 			elif debug.split()[0].lower() == 'region_name':
-				print color.ble+"\tRegion name: %s" % color.green+str(geoip(ipaddr=ipadress).regionName())+color.reset
+				print color.ble+"\tRegion name: %s" % color.green+str(iptolocate.regionName())+color.reset
 			elif debug.split()[0].lower() == 'timezone':
-				print color.ble+"\tTime Zone: %s" % color.green+str(geoip(ipaddr=ipadress).timezone())+color.reset
+				print color.ble+"\tTime Zone: %s" % color.green+str(iptolocate.timezone())+color.reset
+			elif debug.split()[0].lower() == 'google_maps':
+				print color.ble+"\tGoogle maps: %s" % color.green+str(iptolocate.google_maps())+color.reset
+			elif debug.split()[0].lower() == 'all_information':
+				welcome = "\nGeolocation to %s:" % (str(ipadress))
+				for i in range(int(len(welcome))-1):
+					welcome_char = "#"
+				print welcome_char+"\n"
+							
+				ass = iptolocate.association()
+				country = iptolocate.country()
+				city = iptolocate.city()
+				countryCode = iptolocate.countryCode()
+				isp = iptolocate.isp()
+				lat = iptolocate.latitude()
+				lon = iptolocate.longitude()
+				org = iptolocate.organization()
+				query = iptolocate.query()
+				region = iptolocate.region()
+				regionName = iptolocate.regionName()
+				timezone = iptolocate.timezone()
+				zipcode = iptolocate.zipCode()
+				google_maps = iptolocate.google_maps()
+					
+				list_isp_query = {country,city,countryCode,isp,org,lat,lon,query,ass,region,regionName,timezone,zipcode,google_maps}
+							
+				for i in list_isp_query:
+					if i == '':
+						i = None
+							
+				print "\n"
+				print "\tCountry(%s) \t\t:: %s" % (str(countryCode),str(country))
+				print "\tCity \t\t\t:: %s" % (str(city))
+				print "\tISP \t\t\t:: %s" % (str(isp))
+				print "\tOrganization \t\t:: %s" % (str(org))
+				print "\tLatitude \t\t:: %s" % (str(lat))
+				print "\tLongitude \t\t:: %s" % (str(lon))
+				print "\tQuery \t\t\t:: %s" % (str(query))
+				print "\tAssociation \t\t:: %s" % (str(ass))
+				print "\tRegion \t\t\t:: %s" % (str(region))
+				print "\tRegion Name \t\t:: %s" % (str(regionName))
+				print "\tTime Zone \t\t:: %s" % (str(timezone))
+				print "\tZip Code \t\t:: %s" % (str(zipcode))
+				print "\tGoogle maps \t\t:: %s" % (str(google_maps))
+				print "\n"
+		else:
+			print color.adv+"The ip address is not defined."
 			
 	except EOFError:
 		print color.adv+"Invalid key ..."
